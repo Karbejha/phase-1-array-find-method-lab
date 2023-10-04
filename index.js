be3190af1066194cde00d4aa741d57b626b3d46f
Mohamad Karbejha <78583194+Karbejha@users.noreply.github.com>
2023-10-04 12:08:15 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function superbowlWin(record) {
+    const winRecord = record.find(game => game.result === "W");
+
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+
+  // Example usage:
+  const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ];
+
+  const yearOfWin = superbowlWin(record);
+  console.log(yearOfWin); // This will print the year of the win or undefined if no win is found.
